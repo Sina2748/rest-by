fd662962c33264d8a518cefd33441f77fce614d5
Flutter PC <sina.sam@gmail.com>
2022-08-09 18:52:33 +0430

    gyg

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "My First Gatsby Site, RestBy",
   },
-  plugins: [`gatsby-plugin-sass`],
-}
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
+};
